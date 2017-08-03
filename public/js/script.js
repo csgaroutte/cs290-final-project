4badcdb74d92b816a282705e0f907e8e6de62ac9
Chris Garoutte <chrisgaroutte@outlook.com>
2017-08-03 19:21:36 -0400

    insert query

--- public/js/script.js
+++ public/js/script.js
@@ -1,18 +1,38 @@
 "use strict";
 var host = "/new";
 
+function validateNewEntry(form){
+    var reps = form.reps.value;
+    var weight = form.weight.value;
+    var units = form.lbs.value;
+    
+    //validate here
+}
+
 window.addEventListener("load", function(){
     document.querySelector("#newExerciseForm").addEventListener("submit", function(event){
         event.preventDefault();
+
+        var form = document.querySelector("#newExerciseForm");
+        validateNewEntry(form); 
+        
+        var params = {};
+        for(var i = 0; i < form.elements.length - 1; i++){
+            params[form.elements[i].name] = form.elements[i].value;
+        };
+
+        console.log(params);
+
         var req = new XMLHttpRequest(); 
-        req.open("GET", host + "?a=b", true);
+        req.open("POST", host, true);
+        req.setRequestHeader('Content-Type', 'application/json');
         req.addEventListener("load", function(event){
             if(req.readyState == 4 && req.status >= 200 && req.status < 400){
-                console.log("AJAX success!");
+                console.log(req.responseText);
             } else {
                 console.log("Something isn't right Error: " + req.status + ".");
             }
         });
-        req.send(null);
+        req.send(JSON.stringify(params));
     });
 });
--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var app = express();
 
+var handlebars = require('express-handlebars').create({defaultLayout: "main"});
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
 
 var path = require('path');
 app.use(express.static(path.join(__dirname, 'public')));
 
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 var mysql = require('mysql');
 var pool = mysql.createPool({
@@ -16,9 +22,25 @@ var pool = mysql.createPool({
 
 app.set('port', 50918);
 
-app.get("/new", function(req, res){
-    console.log("GOT A REQUEST");
-    res.send("asdf");
+app.get("/", function(req, res){
+    var context = {};
+    res.render('index', context);
+});
+
+app.post("/new", function(req, res){
+   var context = {};
+  console.log(req); 
+    mysql.pool.query("INSERT INTO workouts (*) VALUES(?)", req.body.exercise,
+            req.body.reps, req.body.weight, req.body.lbs, req.body.date, 
+           function(err, result, fields){
+              if(err){
+                 throw err;
+              } else {
+                 console.log("values inserted successfully");
+              }
+
+    res.send(context);
+           }); 
 });
 
 
